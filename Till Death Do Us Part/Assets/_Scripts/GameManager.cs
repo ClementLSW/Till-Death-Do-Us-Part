c8d8c8e6c49e3f8550e6b10de95827ff038b0c1e
Clement Leow <clementlsw@gmail.com>
2025-08-03 14:46:17 +0800

    Added more stuff

--- Till Death Do Us Part/Assets/_Scripts/GameManager.cs
+++ Till Death Do Us Part/Assets/_Scripts/GameManager.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public enum GameState
+    {
+        MainMenu,
+        Playing,
+        Paused
+    }
+
+    public GameState CurrentGameState { get; private set; } = GameState.MainMenu;
     public int Score { get; private set; } = 0;
     public DayOfWeek.Day CurrentDay { get; set; } = DayOfWeek.Day.Monday;
     SaveLoad saveLoad;
@@ -40,6 +48,32 @@ public class GameManager : MonoBehaviour
         ResetScore();
         return tempScore;
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && CurrentGameState != GameState.MainMenu)
+        {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        if (CurrentGameState == GameState.Playing)
+        {
+            CurrentGameState = GameState.Paused;
+            Time.timeScale = 0f; // Pause the game
+            Debug.Log("Game Paused");
+        }
+        else if (CurrentGameState == GameState.Paused)
+        {
+            CurrentGameState = GameState.Playing;
+            Time.timeScale = 1f; // Resume the game
+            Debug.Log("Game Resumed");
+        }
+    }
+
+
     public void QuitGame()
     {
 #if UNITY_EDITOR
--- Till Death Do Us Part/Assets/_Scripts/SettingsManager.cs
+++ Till Death Do Us Part/Assets/_Scripts/SettingsManager.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public AudioManager audioManager;
+    public AudioMixer audioMixer;
+
+    const string MASTER_KEY = "MasterVolume";
+    const string MUSIC_KEY = "MusicVolume";
+    const string SFX_KEY = "SFXVolume";
+
+    private void Awake()
     {
-        
+#pragma warning disable CS0618 // Type or member is obsolete
+        AudioManager audioManager = FindObjectOfType<AudioManager>();
+#pragma warning restore CS0618 // Type or member is obsolete
+        audioMixer = audioManager.audioMixer;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        LoadVolumeSettings();
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(MASTER_KEY, volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(MUSIC_KEY, volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(SFX_KEY, volume);
+    }
+
+    public void LoadVolumeSettings()
+    {
+        float master = PlayerPrefs.GetFloat(MASTER_KEY, 1f);
+        float music = PlayerPrefs.GetFloat(MUSIC_KEY, 1f);
+        float sfx = PlayerPrefs.GetFloat(SFX_KEY, 1f);
+
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(master) * 20);
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(music) * 20);
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(sfx) * 20);
     }
 }
